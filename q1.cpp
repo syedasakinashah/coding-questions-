6b374b3499e4f730291df56ea8caaa6588409da4
syeda_sakina_shahzadi_bukhari <sakina0001w@gmail.com>
2026-06-11 06:44:41 +0000

    update

---
+++ q1.cpp
@@ -0,0 +1,5 @@
+/* Write a program that calculates the acerage score of a srudent
+ for five ecams, dsplaing the results in decial notaion waith one 
+ decimal piace. the program should promptthe user to enter the score 
+ ( out of 100)for each exam.*/
+  
