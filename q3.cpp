e22949b4a972321e82839d46476f875dea2e8bf9
syeda_sakina_shahzadi_bukhari <sakina0001w@gmail.com>
2026-06-14 11:26:26 +0500

    ans

--- q3.cpp
+++ q3.cpp
@@ -1,1 +1,25 @@
 
+/*Calculate and display a table showing the distance traveled by a river 
+flowing at a rate of 2.3 meters per second. The table should include the 
+distance traveled for each second up to 25 seconds.*/
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+
+    float speed=2.3;
+    float distance;
+
+    cout<< "Time\t\t distance"<<endl;
+    cout << "------------------------" << endl;
+    for(int time=1;time<=25;time++)
+    {
+        distance = speed*time;
+        cout << time << "\t\t" << fixed << setprecision(1) << distance << endl;
+
+
+    }
+    return 0;
+}
