1ae188c6e4a699bcb92cc50f8fa7cc7a9485f92d
syeda_sakina_shahzadi_bukhari <sakina0001w@gmail.com>
2026-06-14 12:03:13 +0500

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age, experience;
+    int lift, wpm;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Enter years of experience: ";
+    cin >> experience;
+
+    cout << "Enter maximum weight you can lift (pounds): ";
+    cin >> lift;
+
+    cout << "Enter typing speed (words per minute): ";
+    cin >> wpm;
+
+    if ((age >= 20 || experience >= 5) &&
+        (lift >= 25 || wpm >= 60))
+    {
+        cout << "Accept";
+    }
+    else
+    {
+        cout << "Reject";
+    }
+
+    return 0;
+}
